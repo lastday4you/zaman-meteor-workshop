18df5b0e179a1104f9ea686a547862912f462196
keang hok <laykeanghok@gmail.com>
2017-03-13 13:54:42 +0700

    edit and delete

--- client/showforum.js
+++ client/showforum.js
@@ -1,8 +1,33 @@
 import {Forum} from '../lib/forum';
 
+Template.showForum.onCreated(function () {
+  Session.set('edit-item-id', null);
+});
 
 Template.showForum.helpers({
   forums (){
     return Forum.find();
+  }
+});
+
+Template.showForum.events({
+  'click .rmbt'(event, template){
+    console.log(this);
+    Forum.remove({_id: this._id});
+  },
+  'click .edbt' (event, template){
+    console.log( $('#e-content'));
+    Session.set('edit-item-id', this._id);
+    $('#e-content').val(this.content);
+  },
+  'submit #editstatus'(event, template){
+    event.preventDefault();
+    console.log(Session.get('edit-item-id'));
+    Forum.update(
+      {_id: Session.get('edit-item-id')}
+      , {
+        $set : {content: event.target.econtent.value}
+      }
+      );
   }
 });
